aa6224fdd979a1e94e3209b78c8e594a723a907e
Imane <imaneelmouradi98@gmail.com>
2020-01-11 16:50:11 +0100

    add TextInput

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Imane");
+  const [age, setAge] = useState("21");
 
   const clickHandler = () => {
-    setName("amine");
+    setName("Laura");
+    setAge("27");
   };
 
   return (
     <View style={styles.container}>
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="e.g. john doe"
+        onChangeText={value => setName(value)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+        placeholder="e.g. 21"
+        onChangeText={value => setAge(value)}
+      />
       <Text>My name is {name}</Text>
-      <Text></Text>
+      <Text>My age is {age}</Text>
 
       <View style={styles.buttonContainer}>
         <Button title="update name" onPress={clickHandler} />
@@ -30,6 +46,14 @@ const styles = StyleSheet.create({
 
   buttonContainer: {
     marginTop: 20
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 5,
+    width: 200
   }
   // header: {
   //   backgroundColor: "pink",
